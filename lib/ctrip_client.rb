cb3fa4b286030e60b6a76ad9723f9db72266465b
Roy-Mao <ruoyumao@yahoo.com>
2023-10-05 18:03:41 +0900

    Initial commit

---
+++ lib/ctrip_client.rb
@@ -0,0 +1,11 @@
+require "ctrip_client/version"
+
+module CtripClient
+  class Error < StandardError; end
+  # Your code goes here...
+  class API
+    def initialize(attribute)
+      @attribute = attribute
+    end
+  end
+end
---
+++ lib/ctrip_client/version.rb
@@ -0,0 +1,3 @@
+module CtripClient
+  VERSION = "0.1.0"
+end
