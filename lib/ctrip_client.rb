43caa63b10a1dae1113ae6507a60b11602a41d5d
Roy-Mao <ruoyumao@yahoo.com>
2023-10-05 18:14:04 +0900

    rubocop -A

--- lib/ctrip_client.rb
+++ lib/ctrip_client.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'ctrip_client/version'
 
 module CtripClient
--- spec/ctrip_client_spec.rb
+++ spec/ctrip_client_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe CtripClient do
   it 'has a version number' do
     expect(CtripClient::VERSION).not_to be nil
